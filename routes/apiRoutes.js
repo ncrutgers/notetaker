117b8aed9a28f7f72abd5c4520dda1c45ca67e0e
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-05-29 16:40:44 -0400

    App.delete & post script changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,53 +3,68 @@ var path = require("path");
 
 module.exports = function(app) {
 
-// The app has a backend `db.json` file used to store and retrieve notes using the `fs` module.
-// Reads the `db.json` file and returns all saved notes as JSON.
-app.get("/api/notes", function(req, res) { 
-    // returns string response
-  fs.readFile("./db/db.json", "utf8", function(err, response) {
-      if (err) throw err;
-      // returns JSON object 
-      res.json(JSON.parse(response));
-    });
-  });
-
   // The app has a backend `db.json` file used to store and retrieve notes using the `fs` module.
-// Receives a new note to save on the request body, adds it to the `db.json` file, and then returns the new note to the client.
-app.post("/api/notes", function(req, res) {
-    // saves object {} to newNote
-    var newNote = req.body;
-    // console.log(newNote);
-    fs.readFile("./db/db.json", "utf8", function(err, data) {
-      if (err) throw err;      
-      var allNotes = JSON.parse(data);
-      allNotes.push(newNote);
-
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), function(err) {
+  // Reads the `db.json` file and returns all saved notes as JSON.
+  app.get("/api/notes", function(req, res) { 
+      // returns string response
+    fs.readFile("./db/db.json", "utf8", function(err, response) {
         if (err) throw err;
-        res.json(newNote);
-      }); 
-    }); 
-});
-
-// Receives a query parameter containing the id of a note to delete. Each note has a unique `id` when it's saved. To delete a note, read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-app.delete("/api/notes/:id", function(req, res) {
-  var paramId = req.params.id;
-  fs.readFile("./db/db.json", "utf8", function(err, data) {
-    if (err) throw err;
-    var allNotes = JSON.parse(data);
-    for (var i = 0; i < allNotes.length; i++) {
-      
-      if (paramId === allNotes[i].id) {
-        allNotes.splice(i, 1);
-      }
-    }
-    fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), function(err) {
-      if (err) throw err;      
-    });
-
-    //res.json();
+        // returns JSON object 
+        res.json(JSON.parse(response));
+      });
   });
-});
 
-};
+    // The app has a backend `db.json` file used to store and retrieve notes using the `fs` module.
+  // Receives a new note to save on the request body, adds it to the `db.json` file, and then returns the new note to the client.
+  app.post("/api/notes", function(req, res) {
+      // saves object {} to newNote
+      var newNote = req.body;
+      //console.log(newNote);
+      
+      fs.readFile("./db/db.json", "utf8", function(err, data) {
+        if (err) throw err;      
+        
+        allNotes = JSON.parse(data);   
+        if (allNotes.length){
+          //console.log("True");
+          for (var i = 0; i < allNotes.length; i++){
+              newNote.id = allNotes[i].id + 1;
+          }
+        } else {
+          //console.log("false");
+          newNote.id = 1;
+        } 
+
+        allNotes.push(newNote);
+        //console.log(allNotes);
+
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), function(err) {
+          if (err) throw err;
+          res.json(newNote);
+        }); 
+      }); 
+  });
+
+  // Receives a query parameter containing the id of a note to delete. Each note has a unique `id` when it's saved. To delete a note, read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+  app.delete("/api/notes/:id", function(req, res) {
+    var paramId = req.params.id;
+    //console.log(paramId);
+    fs.readFile("./db/db.json", "utf8", function(err, data) {
+      if (err) throw err;
+      var allNotes = JSON.parse(data);
+      
+      // console.log(typeof allNotes[2].id);//number
+      // console.log(typeof paramId);//string
+      for (var i = 0; i < allNotes.length; i++) {        
+        if (parseInt(paramId) === allNotes[i].id) {
+          allNotes.splice(i, 1);          
+        }
+      }
+      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), function(err) {
+        if (err) throw err;      
+      });
+      res.json(allNotes);
+    });
+  });
+
+}
